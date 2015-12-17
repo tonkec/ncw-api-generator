58773fde2917a2cda7cf4068fc075801de6b0a4e
antonija_simic <antonija1023@gmail.com>
2015-12-17 21:57:15 +0100

    generate controller for users; show all users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get 'users/index'
 
+  get 'static_pages/home'
   devise_for :users
   resources :typographies
   root "static_pages#home"
@@ -9,4 +10,5 @@ Rails.application.routes.draw do
     get "sign_up", to: "devise/registrations#new"
     delete "sign_out", to: "devise/sessions#destroy"
   end
+  resources :users, :only => [:index]
 end
